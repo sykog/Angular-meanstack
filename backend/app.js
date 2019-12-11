2acba537411ea8f5f850c67ea0b9f0306e89d13a
sykog <35271885+sykog@users.noreply.github.com>
2019-12-11 16:30:55 -0500

    set up CRUD options for db

---
+++ backend/app.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let path = require('path');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let dataBaseConfig = require('./database/db');
+
+// Connecting mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(dataBaseConfig.db, {
+  useNewUrlParser: true
+}).then(() => console.log('Database connected sucessfully '),
+  error => console.log('Could not connected to database : ' + error)
+);
+
+// Set up express js port
+const studentRoute = require('../backend/routes/student.route')
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/angular8-meanstack-angular-material')));
+app.use('/', express.static(path.join(__dirname, 'dist/angular8-meanstack-angular-material')));
+app.use('/api', studentRoute);
+
+// Create port
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// Find 404 and hand over to error handler
+app.use((request, response, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (error, request, response, next) {
+  console.error(error.message);
+  if (!error.statusCode) error.statusCode = 500;
+  response.status(error.statusCode).send(error.message);
+});
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,55 @@
+const expresponses = require('expresponses');
+const app = expresponses();
+const userRoute = expresponses.Router();
+
+// User model
+let User = require('../model/User');
+
+// Add User
+userRoute.route('/add-user').post((request, response, next) => {
+  User.create(request.body, (error, data) => {
+    if (error) return next(error);
+    else response.json(data);
+  });
+});
+
+// Get all users
+userRoute.route('/').get((request, response) => {
+  User.find((error, data) => {
+    if (error) return next(error);
+    else response.json(data);
+  });
+});
+
+// Get user by id
+userRoute.route('/read-user/:id').get((request, response) => {
+  User.findById(request.params.id, (error, data) => {
+    if (error) return next(error);
+    else response.json(data);
+  });
+});
+
+// Update user
+userRoute.route('/update-user/:id').put((request, response, next) => {
+  User.findByIdAndUpdate(request.params.id, {
+    $set: request.body
+  }, (error, data) => {
+    if (error) {
+      return next(error);
+      console.log(error);
+    } else {
+      response.json(data);
+      console.log('User successfully updated!');
+    }
+  });
+});
+
+// Delete user
+userRoute.route('/delete-user/:id').delete((request, response, next) => {
+  User.findByIdAndRemove(request.params.id, (error, data) => {
+    if (error) return next(error);
+    else response.status(200).json({msg: data});
+  });
+});
+
+module.exports = userRoute;
