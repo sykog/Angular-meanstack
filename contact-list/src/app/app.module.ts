c6d2f1c8d8e3ecae0dbcde365b2f5482e53a08ef
sykog <35271885+sykog@users.noreply.github.com>
2019-12-12 15:08:13 -0500

    user pagination

--- contact-list/src/app/app.module.ts
+++ contact-list/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './admin/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './admin/home/home.component';
 import {ApiService} from "./services/api.service";
-import { AddUserComponent } from './user/add-user/add-user.component';
-import { UserListComponent } from './user/user-list/user-list.component';
+import {AddUserComponent} from './user/add-user/add-user.component';
+import {UserListComponent} from './user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +23,7 @@ import { UserListComponent } from './user/user-list/user-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     NgbModule,
     HttpClientModule
@@ -30,4 +31,5 @@ import { UserListComponent } from './user/user-list/user-list.component';
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
